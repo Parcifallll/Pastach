cf74c70d6fe98b44f25e3672179e46bd5b7d56cc
Parcifal <89307076692roman@gmail.com>
2025-09-15 13:34:14 +0300

    Initial commit

---
+++ src/main/java/com/example/Pastach/PastachApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pastach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pastach/PastachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pastach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
