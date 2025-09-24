e11ddc8fb5225996633ef35996158e38f0d6afff
Parcifal <89307076692roman@gmail.com>
2025-09-24 11:22:15 +0300

    feat(service):divide controllers and services for users and posts

--- src/main/java/com/example/Pastach/controller/PostController.java
+++ src/main/java/com/example/Pastach/controller/PostController.java
@@ -1,26 +1,32 @@
 package com.example.Pastach.controller;
 
 import com.example.Pastach.model.Post;
+import com.example.Pastach.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PostController { //manage Posts
-    private List<Post> posts = new ArrayList<>();
+    private final PostService postService;
 
-    @GetMapping("/posts")
-    public List<Post> findAll() {
-        return posts;
+    @Autowired
+    public PostController(PostService postService) { // DI
+        this.postService = postService;
     }
 
-    @PostMapping(value = "/post")
+
+    @GetMapping("/posts") // endpoint
+    public List<Post> findAll() {
+        return postService.findAll();
+    }
+
+
+    @PostMapping(value = "/post") // endpoint
     public Post create(@RequestBody Post post) {
-        posts.add(post);
-        return post;
+        return postService.create(post);
     }
 }
