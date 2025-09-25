7eda05fbab41d230763e7bfe54addd1be12dd7a5
Parcifal <89307076692roman@gmail.com>
2025-09-25 22:16:21 +0300

    feat(controller): add ErrorHandler with handlers

---
+++ src/main/java/com/example/Pastach/controller/ErrorHandler.java
@@ -0,0 +1,60 @@
+package com.example.Pastach.controller;
+
+import com.example.Pastach.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.example.Pastach.controller.")
+@ResponseBody
+public class ErrorHandler {
+    public static class ErrorResponse {
+        private final String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlePostNotFoundException(final PostNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFoundException(final UserNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT) // 409 code-status
+    public ErrorResponse handleUserAlreadyExistException(final UserAlreadyExistException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidEmailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidEmailException(final InvalidEmailException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(IncorrectParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIncorrectParameterException(final IncorrectParameterException e) {
+        return new ErrorResponse(String.format("Mistake in field \"%s\".", e.getMessage()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+    public ErrorResponse handleThrowable (final Throwable e) {
+        return new ErrorResponse("Something went wrong...");
+    }
+}
