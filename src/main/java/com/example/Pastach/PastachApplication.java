e8a5b27541a9852af088a5ed001f85a672f0185b
Parcifal <89307076692roman@gmail.com>
2025-10-09 11:18:02 +0300

    docs(sealed): add sealed classes

--- src/main/java/com/example/Pastach/PastachApplication.java
+++ src/main/java/com/example/Pastach/PastachApplication.java
@@ -3,6 +3,10 @@ package com.example.Pastach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class PastachApplication {
 
