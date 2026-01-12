3a677c2dc5c7d033bd79cd504bf5aba82bb0ecfb
Parcifal <89307076692roman@gmail.com>
2026-01-12 19:31:26 +0300

    fix: change type of id - VARCHAR to BIGINT, add users.username

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -7,38 +7,41 @@ CREATE TABLE IF NOT EXISTS public.roles
 INSERT INTO public.roles (name)
 SELECT 'USER'
 WHERE NOT EXISTS (SELECT 1 FROM public.roles WHERE name = 'USER');
+
 INSERT INTO public.roles (name)
 SELECT 'ADMIN'
 WHERE NOT EXISTS (SELECT 1 FROM public.roles WHERE name = 'ADMIN');
 
 CREATE TABLE IF NOT EXISTS public.users
 (
-    id         VARCHAR(255) PRIMARY KEY, --PK always created with index
+    id         BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
-    birthday   DATE         NOT NULL,
+    birthday   DATE,
     locked     BOOLEAN               DEFAULT FALSE,
     created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX IF NOT EXISTS idx_users_email ON public.users(email);
-
 CREATE TABLE IF NOT EXISTS public.posts
 (
     id         BIGSERIAL PRIMARY KEY,
-    author_id  VARCHAR(255) NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
+    author_id  BIGINT      NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
     text       TEXT,
     photo_url  VARCHAR(255),
-    created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS idx_posts_author_id ON public.posts(author_id);
+CREATE INDEX IF NOT EXISTS idx_posts_created_at ON public.posts(created_at DESC);
 
 CREATE TABLE IF NOT EXISTS public.user_roles
 (
-    user_id VARCHAR(255) NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
-    role_id BIGSERIAL      NOT NULL REFERENCES public.roles (id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
+    role_id BIGINT NOT NULL REFERENCES public.roles (id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, role_id)
 );
+
+CREATE INDEX IF NOT EXISTS idx_user_roles_role_id ON public.user_roles(role_id);
